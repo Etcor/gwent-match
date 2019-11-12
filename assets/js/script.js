b5bf51ff0c90106473018c3996c82ad74960954d
Ethan Cordes <ethancordes7@gmail.com>
2019-11-12 10:08:02 -0800

    made initial skeleton placed elements roughly MVP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+$(document).ready(initializeApp);
 
+function initializeApp(){
+  console.log('Loaded...');
+  var cardElement = $('.card');
+  cardElement.on('click', flipCard);
+}
+
+function flipCard(){
+  $(this).find('.back').toggle();
+}
