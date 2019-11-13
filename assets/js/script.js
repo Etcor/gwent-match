b4dd64a85ac489b3e5238972b3563bbec1cf1f98
Ethan Cordes <ethancordes7@gmail.com>
2019-11-12 23:27:13 -0800

    added the rest of the card front img added attempts and accuracy as dynamic dom elements small other syntax fixes and comment additions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,48 +2,51 @@ $(document).ready(initializeApp);
 
 var firstCardClicked = null;
 var secondCardClicked = null;
-var matches = null;
-var gamesPlayed = null;
-var attempts = null;
+var matches = null; //increment after every successful match
+var gamesPlayed = null; //increment after all cards have been matched
+var attempts = null;//increment after an attempted match
+
 
 function initializeApp(){
-  console.log('Loaded...');
   var cardElement = $('.card');
-  cardElement.on('click', handleCardClick);
+  cardElement.on('click', handleCardClick); //click handler for cards
 }
 
-function resetStats(){
-  gamesPlayed += 1;
-  matches = null;
-  attempts = null;
-}
+// function resetStats(){
+//   gamesPlayed += 1;
+//   matches = null;
+//   attempts = null;
+// }
 
 function handleCardClick(event){
-  console.log(event);
-  var currentTarget = $(event.currentTarget);
-  currentTarget.find('.back').addClass('hidden');
-  if(firstCardClicked === null){
+  var currentTarget = $(event.currentTarget); //selects the card that is clicked
+  currentTarget.find('.back').addClass('hidden'); //hides the back of the card, revealing the face
+
+  if(firstCardClicked === null){ // uses previous selector to assign clicked card to variable
     firstCardClicked = currentTarget;
-  }else/* if(secondCardClicked === null)*/{
+  }else{
     secondCardClicked = currentTarget;
   }
-  if(firstCardClicked !== null && secondCardClicked !== null){
-    var firstCardImage = firstCardClicked.find('.front').css('background-image');
-    var secondCardImage = secondCardClicked.find('.front').css('background-image');
-    if(firstCardImage !== secondCardImage){
-        console.log('No match');
-        setTimeout(function () {
+  // debugger;
+  if(firstCardClicked !== null && secondCardClicked !== null){ //conditional checking if both cards have been clicked
+    attempts += 1;//on any match attempt, increments attemps var
+    var firstCardImage = firstCardClicked.find('.front').css('background-image');//finds bg img for comp.
+    var secondCardImage = secondCardClicked.find('.front').css('background-image');//finds bg img for comp.
+    if(firstCardImage !== secondCardImage){//checks if they are the same img
+        setTimeout(function () {//timer to reset css on card if img doesnt match
           firstCardClicked.find('.back').removeClass('hidden');
           secondCardClicked.find('.back').removeClass('hidden');
           firstCardClicked = null;
           secondCardClicked = null;
-        }, 1500);
-      }else{
-      console.log('They match');
-      firstCardClicked = null;
-      secondCardClicked = null;
-      matches += 1;
+          }, 1500);
+      }else{ //resets clicked card variables on success or failure
+        firstCardClicked = null;
+        secondCardClicked = null;
+        matches += 1; //on successful match, increments match var
     }
-    attempts += 1;
+    var accuracy = matches / attempts;
+    var accuracyPercentage = Math.round(accuracy * 100); //calculates accuracy of user
+    $('#attemptsNumber').text(attempts);
+    $('#accuracyNumber').text(accuracyPercentage + '%');
   }
 }
